35ed5761cdf51c31317efc425710265db7a9acd2
thddb <thddb@YUNA-GB3>
2023-08-29 15:57:33 +0900

    select 기본 & 과제 업로드

---
+++ sql연습/select-기본.sql
@@ -0,0 +1,12 @@
+-- SELECT 연습 (Limit to 1000 rows)
+
+-- 예제 1. departments 테이블의 모든 데이터를 출력
+select *from departments;
+
+-- 프로젝션 (projection)
+-- 예제 2. employees 테이블에서 직원 이름, 성별, 입사일을 출력
+select first_name as 이름, gender as 성별, hire_date as 입사일 from employees;
+
+-- distinct
+-- 예제 3. titles 테이블에서 모든 직급을 출력
+select distinct title from titles;
